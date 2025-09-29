78b4c041d212f98f463f84e58398c57c11f90325
MayoralAJ <ana.mayoral.1bmangal@gmail.com>
2025-09-29 13:39:10 -0300

    Update m1.js

--- M2/M2.2/m1.js
+++ M2/M2.2/m1.js
@@ -1,4 +1,5 @@
 //### **sando `length`**
 //Crie um array chamado `frutas` com três frutas. Exiba no console o número total de elementos usando `length`.
-var frutas = ["maçã", "banana", "laranja"];
+var frutas = ["goiaba", "banana", "mamão"];
+
 console.log(frutas.length); 
